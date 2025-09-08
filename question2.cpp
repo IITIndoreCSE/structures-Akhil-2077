2b4579ddbc816642782726b704aa5641c018d87b
Akhil-2077 <ee250002077@iiti.ac.in>
2025-09-08 15:57:11 +0530

    Update question2.cpp

--- question2.cpp
+++ question2.cpp
@@ -4,16 +4,16 @@
 
 using namespace std;
 
+// Define the Rectangle struct
 struct Rectangle {
-double width;
-double height;
-double density;
-string material;
-double mass;
-};// TODO: Define the Rectangle struct according to README
-// struct Rectangle {
-// };
+    double width;
+    double height;
+    double thickness;
+    double density;
+    char material[100];
+};
 
+// Output function to print plate details
 void print_plate(int index, double width, double height,
                  double density, double mass, const char* material) {
     cout << "Plate[" << index << "]: "
@@ -41,28 +41,27 @@ int main(int argc, char* argv[]) {
     input.ignore(); // skip newline after number
 
     const int MAX_PLATES = 10;
-Rectangle plates[MAX_PLATES];
 
-  for (int i = 0; i < num_plates; i++) {
-    input >> plates[i].width >> plates[i].height >> plates[i].density;
-    input.ignore();
-    getline(input, plates[i].material);
+    // Create an array of Rectangle structs
+    Rectangle plates[MAX_PLATES];
 
-    plates[i].mass = plates[i].width * plates[i].height * plates[i].density;
-  }
-  for (int i = 0; i < num_plates; i++) { 
-  print_plate(i + 1, plates[i]);
-  }
-    // TODO: Create an array of Rectangle
-    // Rectangle plates[MAX_PLATES];
+    // Read plate data from input
+    for (int i = 0; i < num_plates; i++) {
+        input >> plates[i].width;
+        input >> plates[i].height;
+        input >> plates[i].thickness;
+        input >> plates[i].density;
+        input.ignore();  // Skip the newline after density
+        input.getline(plates[i].material, 100);  // Read the material name
+    }
 
-    // TODO: Read plate data from input
-    // for (int i = 0; i < num_plates; i++) {
-    // }
-
-    // TODO: Compute mass for each plate and call print_plate
-    // for (int i = 0; i < num_plates; i++) {
-    // }
+    // Compute mass for each plate and call print_plate
+    for (int i = 0; i < num_plates; i++) {
+        // Calculate mass: mass = width * height * thickness * density
+        double mass = plates[i].width * plates[i].height * plates[i].thickness * plates[i].density;
+        print_plate(i, plates[i].width, plates[i].height, plates[i].density, mass, plates[i].material);
+    }
 
     return 0;
 }
+
